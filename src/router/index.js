85edf85a198d64edf228bf89ab716138ee172b0f
niuxiaoya <1179039814@qq.com>
2018-04-27 15:13:41 +0800

    'change'

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/Index'
 
-
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +10,6 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
-    },
+    }
   ]
 })
